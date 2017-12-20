1dc2071acb2a39d40cbd9334d48a926ef514c024
Terren Peterson <terrenjpeterson@yahoo.com>
2017-12-20 15:36:40 +0000

    added deployment tools

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# create build package and deploy a new skill
+
+# create temp zip file with build package contents
+zip -r drinkbot.zip index.js package.json node_modules/
+
+# stage the temp file in s3
+aws s3 cp drinkbot.zip s3://drinkrecommender/binaries/
+
+# remove the temporary file
+rm drinkbot.zip
+
+# update the lambda function with the binaries that have been staged
+aws lambda update-function-code --function-name drinkRecommendationGreen --s3-bucket drinkrecommender --s3-key binaries/drinkbot.zip
